2ffff8baca0cdb1ede26cb2f7810d1d092fdd780
luuthevinh1988@gmail.com <luuthevinh1988@gmai.com>
2025-08-15 16:32:33 +0700

    initialize the project and install packages

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompres = "Hello Factipjs";
+
+  return res.status(200).json({
+    message: "Welcome Fantipjs!",
+    metadata: strCompres.repeat(10000),
+  });
+});
+
+// handling error
+
+module.exports = app;
